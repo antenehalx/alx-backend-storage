bacceec61fb44094cece05ac87c364aa1c047e8e
Antenehalx <anteeyee@gmail.com>
2023-10-18 04:59:33 -0700

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
-
+'''A module with tools for request caching and tracking.
 '''
-    Web cache and tracker
-'''
-
-import requests
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
     '''
-        Returns HTML content from a url
-    '''
-    res = requests.get(url)
-    store.incr('count:{}'.format(url))
-    store.expire('count:{}'.format(url), 10)
-    return res.text
+    return requests.get(url).text
